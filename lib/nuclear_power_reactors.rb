9299c11ad92825e599adf399e096c60173a62b7f
satub <satu.barnhill@gmail.com>
2016-07-26 15:35:10 -0400

    Added file placeholders to bin and lib folders

---
+++ lib/nuclear_power_reactors.rb
@@ -0,0 +1,16 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+class NuclearPowerReactors
+
+  def initialize
+  end
+
+  def scrape_available_countries
+  end
+
+  def scrape_avalable_reactors
+  end
+
+end
