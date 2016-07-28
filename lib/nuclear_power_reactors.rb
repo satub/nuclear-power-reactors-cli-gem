ed5121a3496149273c4e6011aed44898946901e6
satub <satu.barnhill@gmail.com>
2016-07-28 11:13:25 -0400

    added basic formatting to #list_all_countries

--- lib/nuclear_power_reactors.rb
+++ lib/nuclear_power_reactors.rb
@@ -14,12 +14,27 @@ class NuclearPowerReactors
     @reactor_hash = @npr.scrape_available_reactors
   end
 
-  #needs some basic formatting
   def list_all_countries
+    column_width = @country_hash.keys.max_by {|country_name| country_name.length}.length + 4
+    columns = 3
     @country_hash.keys.each_with_index do |country_name, i|
-      puts "#{i+1}. #{country_name}"
+      if country_name.match(/\,/).nil?
+        format_name = "#{i+1}. #{country_name}"
+      else
+        format_name= "#{i+1}. #{country_name.split(", ").reverse.join(" ")}"
+      end
+      if (i+1) % columns == 0
+        puts "#{format_name.strip}"
+      else
+        (column_width - format_name.length).times do
+          format_name << " "
+        end
+        print "#{format_name}"
+      end
     end
+    puts
   end
+
 
   #Showing all reactors (there are over 660 of them currently) will require heavy formatting. method TBA
   def list_all_reactors
