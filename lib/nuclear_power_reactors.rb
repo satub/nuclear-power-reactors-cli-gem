372641e27121a4167e428f4fcb1b9f7b9e6f54cc
satub <satu.barnhill@gmail.com>
2016-07-28 10:30:50 -0400

    added #list_all_countries

--- lib/nuclear_power_reactors.rb
+++ lib/nuclear_power_reactors.rb
@@ -6,17 +6,22 @@ require 'pry'
 class NuclearPowerReactors
 
   # country and reactor key hashes will link the country and reactor names together with their ids that are used to pull out the right page
-  attr_accessor :country_keys, :reactor_keys, :npr
+  attr_accessor :country_hash, :reactor_hash, :npr
 
   def initialize
     @npr = NPRScraper.new
-    @country_keys = @npr.scrape_available_countries
-    @reactor_keys = @npr.scrape_available_reactors
+    @country_hash = @npr.scrape_available_countries
+    @reactor_hash = @npr.scrape_available_reactors
   end
 
+  #needs some basic formatting
   def list_all_countries
+    @country_hash.keys.each_with_index do |country_name, i|
+      puts "#{i+1}. #{country_name}"
+    end
   end
 
+  #Showing all reactors (there are over 660 of them currently) will require heavy formatting. method TBA
   def list_all_reactors
   end
 
@@ -30,5 +35,5 @@ class NuclearPowerReactors
 end
 
 reactors = NuclearPowerReactors.new
-puts reactors.country_keys
-# puts reactors.reactor_keys
+reactors.list_all_countries
+# puts reactors.reactor_hash
