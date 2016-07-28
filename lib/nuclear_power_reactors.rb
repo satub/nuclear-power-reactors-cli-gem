3c2f2c9c7024427c0801080e1c81a417413a9053
satub <satu.barnhill@gmail.com>
2016-07-28 09:04:12 -0400

    added more placeholders for basic methods

--- lib/nuclear_power_reactors.rb
+++ lib/nuclear_power_reactors.rb
@@ -5,5 +5,17 @@ class NuclearPowerReactors
   def initialize
   end
 
-  
+  def list_all_countries
+  end
+
+  def list_all_reactors
+  end
+
+  def list_reactors_in(country)
+  end
+
+  def show_reactor_details
+  end
+
+
 end
