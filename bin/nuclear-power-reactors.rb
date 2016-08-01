00b5bc910ad5183ee463a8608f8816177c9dc1e2
satub <satu.barnhill@gmail.com>
2016-07-31 21:22:07 -0400

    added looping structure to the cli

--- bin/nuclear-power-reactors.rb
+++ bin/nuclear-power-reactors.rb
@@ -4,6 +4,7 @@ require_relative "../lib/nuclear_power_reactors.rb"
 # require_relative "../lib/npr_scraper.rb"
 # require_relative "../lib/country.rb"
 # require_relative "../lib/reactor.rb"
+require 'pry'
 
 def greet
   puts "Welcome to Nuclear Power Reactors Command Line!"
@@ -12,31 +13,78 @@ def greet
   puts "Data is currently available for the following countries:"
 end
 
-def choose_country
-  puts "Choose a country you want more information on by entering the corresponding two-letter ISO-code."
-  puts "For example, enter FI for Finland. Enter exit to quit the program."
+def get_input
+  puts "Enter a command:"
   input = gets.chomp
 end
 
-def valid_iso_format?(input)
-  !input.match(/\b\w{2}\b/).nil? && input.match(/\d+/).nil?
+def goodbye
+  puts "Thank you for using Nuclear Power Reactors CLI! Have an energetic day! :D"
+end
+
+def choose_country
+  cmd = "none"
+  until valid_iso_format?(cmd) || cmd == "exit"
+    puts "Choose a country you want more information on by entering the corresponding two-letter ISO-code."
+    puts "For example, enter FI for Finland, or enter exit to quit the program:"
+    cmd = gets.chomp
+  end
+  cmd
+end
+
+def choose_reactor
+  cmd = "none"
+  until valid_reactor_id?(cmd) || cmd == "exit"
+    puts "Choose a reactor you want more information on by entering the corresponding id number (number in parenthesis), or enter exit to quit the program:"
+    cmd = gets.chomp
+  end
+  cmd
+end
+
+def list_options
+  puts "Available commandline options: 'help' for this info, 'list' to list all countries, 'country' to choose another country, 'reactor' to choose another reactor, 'search' to go into search mode (TO BE ADDED) or 'exit' to exit the program."
+  get_input
+end
+
+def valid_iso_format?(cmd)
+  !cmd.match(/\b\w{2}\b/).nil? && cmd.match(/\d+/).nil?
+end
+
+def valid_reactor_id?(cmd)
+  cmd.match(/\D/).nil?  #this input should only consist of digits
 end
 
 #run sequence here
-cmd = "go on"
 greet
 npr = NuclearPowerReactors.new
 npr.list_all_countries
+cmd = "country"
 until cmd == "exit" do
-  country = choose_country
-  if valid_iso_format?(country)
-    puts "Yay! You succesfully chose a country!"
-    npr.create_country(country)
-    npr.list_country_data(country)
-    
+  case cmd
+  when "country"
+    cmd = choose_country
+    if cmd == "exit"
+      goodbye
+      exit
+    end
+    npr.create_country(cmd)
+    npr.list_country_data(cmd)
+    cmd = "reactor"
+  when "reactor"
+    cmd = choose_reactor
+    if cmd == "exit"
+      goodbye
+      exit
+    end
+    npr.show_reactor_details(cmd)
+    cmd = list_options
+  when "list"
+    npr.list_all_countries
+    cmd = get_input.downcase
+  when "exit"
+    exit
   else
-    puts "Pay attention, chap!"
+    cmd = list_options
   end
-  cmd = "exit"  #to avoid inifinite loop during testing
 end
-puts "Thank you for using Nuclear Power Reactors CLI! Have an energetic day! :D"
+goodbye
